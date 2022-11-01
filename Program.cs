b88847db48c678783bfa95b9da926e01714e01c8
MrLarin <larindenisandreevich@gmail.com>
2022-11-01 22:05:56 +0200

    Написал код заполнение массива строк

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿
+string[] AS = new string[4];
+for (int i = 0; i < AS.Length; i++)
+{
+    Console.Write("Введите Строку: ");
+    AS[i] = Console.ReadLine()!;
+}
+
+for (int i = 0; i < AS.Length; i++)
+{
+    Console.Write($"{AS[i]} /n ");
+}
